b2716c5b3645712d9fbef973d7fc065913ea7f90
guy.rosin <guy.rosin@893eca86-03a1-db22-0987-cda46d1ef62f>
2011-12-16 23:33:30 +0000

    Added basic GUI (first day's work)

---
+++ trunk/Friendizer-Android/src/com/teamagly/friendizer/MyProfileActivity.java
@@ -0,0 +1,13 @@
+package com.teamagly.friendizer;
+
+import com.teamagly.friendizer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyProfileActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.my_profile_layout);
+	}
+}
