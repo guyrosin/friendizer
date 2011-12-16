595e71b953a8a4924101982ee7e0aeb65e3e1631
guy.rosin <guy.rosin@893eca86-03a1-db22-0987-cda46d1ef62f>
2011-12-16 14:41:25 +0000

    Initial import, 2nd try (for v2.3.3)

---
+++ trunk/Friendizer-Android/src/activities/FriendsActivity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * @author Guy
+ *
+ */
+public class FriendsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textview = new TextView(this);
+        textview.setText("This is the Artists tab");
+        setContentView(textview);
+    }
+
+}
