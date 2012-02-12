c2081eeec04995293a09597a49de7887dd31dff9
aaa.charikar <aaa.charikar@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-02-12 13:21:55 +0000

    Added the new build.

---
+++ Friendizer-AppEngine/src/com/teamagly/friendizer/server/UserLocation.java
@@ -0,0 +1,27 @@
+package com.teamagly.friendizer.server;
+
+public class UserLocation {
+	private double xCord;
+	private double yCord;
+	
+	public UserLocation(double xCord, double yCord) {
+		this.xCord = xCord;
+		this.yCord = yCord;
+	}
+	
+	public double getXCord() {
+		return xCord;
+	}
+	
+	public void setXCord(double xCord) {
+		this.xCord = xCord;
+	}
+	
+	public double getYCord() {
+		return yCord;
+	}
+	
+	public void setYCord(double yCord) {
+		this.yCord = yCord;
+	}
+}
