5211d66280d78110b4e5547f652a488f169b6e6f
guy.rosin <guy.rosin@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-04-11 21:04:15 +0000

    Restoring missing files

---
+++ trunk/Friendizer-Android/src/com/teamagly/friendizer/model/Achievement.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.teamagly.friendizer.model;
+
+public class Achievement {
+    public String title;
+    public String description;
+    public int iconResID;
+    public int reward;
+}
