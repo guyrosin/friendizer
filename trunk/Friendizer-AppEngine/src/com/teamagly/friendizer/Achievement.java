5b5583218ec79f924131817cbf81c74d598afc46
aaa.charikar <aaa.charikar@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-04-13 22:18:18 +0000

    Now the userDetails servlets automatically registers new users. Deleted the register servlet. Added classes for the achievements.

---
+++ trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/Achievement.java
@@ -0,0 +1,70 @@
+package com.teamagly.friendizer;
+
+import javax.jdo.annotations.*;
+
+@PersistenceCapable
+public class Achievement {
+	@PrimaryKey
+	@Persistent
+	private Long id;
+	
+	@Persistent
+    private String title;
+	
+	@Persistent
+    private String description;
+	
+	@Persistent
+    private String iconRes;
+    
+	@Persistent
+	private int reward;
+
+	public Achievement(long id, String title, String description, String iconRes, int reward) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.iconRes = iconRes;
+		this.reward = reward;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getIconRes() {
+		return iconRes;
+	}
+
+	public void setIconRes(String iconRes) {
+		this.iconRes = iconRes;
+	}
+
+	public int getReward() {
+		return reward;
+	}
+
+	public void setReward(int reward) {
+		this.reward = reward;
+	}
+}
--- trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/MarketManager.java
+++ trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/MarketManager.java
@@ -1,7 +1,6 @@
 package com.teamagly.friendizer;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Date;
 import java.util.List;
 
@@ -19,9 +18,7 @@ public class MarketManager extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String address = request.getRequestURI();
 		String servlet = address.substring(address.lastIndexOf("/") + 1);
-		if (servlet.intern() == "register")
-			register(request, response);
-		else if (servlet.intern() == "userDetails")
+		if (servlet.intern() == "userDetails")
 			userDetails(request, response);
 		else if (servlet.intern() == "ownList")
 			ownList(request, response);
@@ -30,55 +27,33 @@ public class MarketManager extends HttpServlet {
 	}
 
 	@SuppressWarnings("unchecked")
-	private void register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		long userID = Long.parseLong(request.getParameter("userID"));
-		PrintWriter out = response.getWriter();
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query query = pm.newQuery(User.class);
-		query.setFilter("id == " + userID);
-		List<User> result = (List<User>) query.execute();
-		query.closeAll();
-		if (!result.isEmpty()) {
-			out.println("This user already exists");
-			return;
-		}
-		User user = new User(userID, 0, 100, 1000, -1, -1, new Date());
-		pm.makePersistent(user);
-		pm.close();
-		out.println("The user was added");
-	}
-
-	@SuppressWarnings("unchecked")
 	private void userDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		long userID = Long.parseLong(request.getParameter("userID"));
-		PrintWriter out = response.getWriter();
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query query = pm.newQuery(User.class);
 		query.setFilter("id == " + userID);
 		List<User> result = (List<User>) query.execute();
 		query.closeAll();
+		User user;
 		if (result.isEmpty()) {
-			out.println("This user doesn't exist");
-			return;
-		}
-		User user = result.get(0);
+			user = new User(userID, 0, 100, 1000, -1, -1, new Date());
+			pm.makePersistent(user);
+		} else
+			user = result.get(0);
 		pm.close();
-		out.println(user);
+		response.getWriter().println(user);
 	}
 
 	@SuppressWarnings("unchecked")
 	private void ownList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		long userID = Long.parseLong(request.getParameter("userID"));
-		PrintWriter out = response.getWriter();
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query query = pm.newQuery(User.class);
 		query.setFilter("id == " + userID);
 		List<User> result = (List<User>) query.execute();
 		query.closeAll();
-		if (result.isEmpty()) {
-			out.println("This user doesn't exist");
-			return;
-		}
+		if (result.isEmpty())
+			throw new ServletException("This user doesn't exist");
 		query = pm.newQuery(User.class);
 		query.setFilter("owner == " + userID);
 		result = (List<User>) query.execute();
@@ -87,14 +62,13 @@ public class MarketManager extends HttpServlet {
 		for (User user : result)
 			users.put(user.toJSONObject());
 		pm.close();
-		out.println(users);
+		response.getWriter().println(users);
 	}
 
 	@SuppressWarnings("unchecked")
 	private void buy(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		long userID = Long.parseLong(request.getParameter("userID"));
 		long buyID = Long.parseLong(request.getParameter("buyID"));
-		PrintWriter out = response.getWriter();
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		Query query = pm.newQuery(User.class);
 		query.setFilter("id == " + userID + " || id == " + buyID);
@@ -107,22 +81,14 @@ public class MarketManager extends HttpServlet {
 			else
 				buy = user;
 		}
-		if (buyer == null) {
-			out.println("This user doesn't exist");
-			return;
-		}
-		if (buy == null) {
-			out.println("The user you want to buy doesn't exist");
-			return;
-		}
-		if (buy.getOwner() == userID) {
-			out.println("You already own the user you want to buy");
-			return;
-		}
-		if (buyer.getMoney() < buy.getValue()) {
-			out.println("You don't have enough money to buy this user");
-			return;
-		}
+		if (buyer == null)
+			throw new ServletException("This user doesn't exist");
+		if (buy == null)
+			throw new ServletException("The user you want to buy doesn't exist");
+		if (buy.getOwner() == userID)
+			throw new ServletException("You already own the user you want to buy");
+		if (buyer.getMoney() < buy.getValue())
+			throw new ServletException("You don't have enough money to buy this user");
 		buyer.setMoney(buyer.getMoney() - buy.getValue());
 		pm.makePersistent(buyer);
 		if (buy.getOwner() > 0) {
@@ -140,6 +106,6 @@ public class MarketManager extends HttpServlet {
 		buy.setOwner(userID);
 		pm.makePersistent(buy);
 		pm.close();
-		out.println("Purchase Done");
+		response.getWriter().println("Purchase Done");
 	}
 }
