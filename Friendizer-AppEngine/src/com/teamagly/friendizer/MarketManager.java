1e5671f9bd98318c2400609d8833a40e228c31a4
guy.rosin <guy.rosin@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-05-19 16:33:31 +0000

    Updated the website with screenshots. Updated App Engine SDK to 1.6.5. Added support for user status

--- Friendizer-AppEngine/src/com/teamagly/friendizer/MarketManager.java
+++ Friendizer-AppEngine/src/com/teamagly/friendizer/MarketManager.java
@@ -1,7 +1,7 @@
 package com.teamagly.friendizer;
 
 import java.io.IOException;
-import java.util.Date;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.jdo.*;
@@ -14,101 +14,132 @@ import com.google.android.c2dm.server.PMF;
 
 @SuppressWarnings("serial")
 public class MarketManager extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String address = request.getRequestURI();
-		String servlet = address.substring(address.lastIndexOf("/") + 1);
-		if (servlet.intern() == "userDetails")
-			userDetails(request, response);
-		else if (servlet.intern() == "ownList")
-			ownList(request, response);
-		else
-			buy(request, response);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String address = request.getRequestURI();
+	String servlet = address.substring(address.lastIndexOf("/") + 1);
+	System.err.println(servlet.intern());
+	if (servlet.intern() == "userDetails")
+	    userDetails(request, response);
+	else if (servlet.intern() == "ownList")
+	    ownList(request, response);
+	else if (servlet.intern() == "buy")
+	    buy(request, response);
+	else if (servlet.intern() == "updateStatus")
+	    updateStatus(request, response);
+    }
 
-	@SuppressWarnings("unchecked")
-	private void userDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		long userID = Long.parseLong(request.getParameter("userID"));
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query query = pm.newQuery(User.class);
-		query.setFilter("id == " + userID);
-		List<User> result = (List<User>) query.execute();
-		query.closeAll();
-		User user;
-		if (result.isEmpty()) {
-			user = new User(userID, 0, 100, 1000, -1, -1, new Date());
-			pm.makePersistent(user);
-		} else
-			user = result.get(0);
-		pm.close();
-		response.getWriter().println(new UserMatching(user, 1));
-	}
+    @SuppressWarnings("unchecked")
+    private void userDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	long userID = Long.parseLong(request.getParameter("userID"));
+	PersistenceManager pm = PMF.get().getPersistenceManager();
+	Query query = pm.newQuery(User.class);
+	query.setFilter("id == " + userID);
+	List<User> result = (List<User>) query.execute();
+	query.closeAll();
+	User user;
+	if (result.isEmpty()) {
+	    user = new User(userID);
+	    pm.makePersistent(user);
+	} else
+	    user = result.get(0);
+	pm.close();
+	response.getWriter().println(new UserMatching(user, 1));
+    }
 
-	@SuppressWarnings("unchecked")
-	private void ownList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		long userID = Long.parseLong(request.getParameter("userID"));
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query query = pm.newQuery(User.class);
-		query.setFilter("id == " + userID);
-		List<User> result = (List<User>) query.execute();
-		query.closeAll();
-		if (result.isEmpty())
-			throw new ServletException("This user doesn't exist");
-		query = pm.newQuery(User.class);
-		query.setFilter("owner == " + userID);
-		result = (List<User>) query.execute();
-		query.closeAll();
-		JSONArray users = new JSONArray();
-		for (User user : result)
-			users.put(user.toJSONObject());
-		pm.close();
-		response.getWriter().println(users);
-	}
+    @SuppressWarnings("unchecked")
+    private void ownList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	long userID = Long.parseLong(request.getParameter("userID"));
+	PersistenceManager pm = PMF.get().getPersistenceManager();
+	Query query = pm.newQuery(User.class);
+	query.setFilter("id == " + userID);
+	List<User> result = (List<User>) query.execute();
+	query.closeAll();
+	if (result.isEmpty())
+	    throw new ServletException("This user doesn't exist");
+	query = pm.newQuery(User.class);
+	query.setFilter("owner == " + userID);
+	result = (List<User>) query.execute();
+	query.closeAll();
+	JSONArray users = new JSONArray();
+	for (User user : result)
+	    users.put(user.toJSONObject());
+	pm.close();
+	response.getWriter().println(users);
+    }
 
-	@SuppressWarnings("unchecked")
-	private void buy(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		long userID = Long.parseLong(request.getParameter("userID"));
-		long buyID = Long.parseLong(request.getParameter("buyID"));
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query query = pm.newQuery(User.class);
-		query.setFilter("id == " + userID + " || id == " + buyID);
-		List<User> result = (List<User>) query.execute();
-		query.closeAll();
-		User buyer = null, buy = null;
-		for (User user : result) {
-			if (user.getId() == userID)
-				buyer = user;
-			else
-				buy = user;
-		}
-		if (buyer == null)
-			throw new ServletException("This user doesn't exist");
-		if (buy == null)
-			throw new ServletException("The user you want to buy doesn't exist");
-		if (buy.getOwner() == userID)
-			throw new ServletException("You already own the user you want to buy");
-		if (buyer.getMoney() < buy.getValue())
-			throw new ServletException("You don't have enough money to buy this user");
-		buyer.setMoney(buyer.getMoney() - buy.getValue());
-		pm.makePersistent(buyer);
-		AchievementsManager.userBoughtSomeone(buyer);
-		if (buy.getOwner() > 0) {
-			query = pm.newQuery(User.class);
-			query.setFilter("id == " + buy.getOwner());
-			result = (List<User>) query.execute();
-			query.closeAll();
-			if (!result.isEmpty()) {
-				User preOwner = result.get(0);
-				preOwner.setMoney(preOwner.getMoney() + buy.getValue());
-				pm.makePersistent(preOwner);
-			}
-		}
-		buy.setValue(buy.getValue() * 11 / 10);
-		buy.setOwner(userID);
-		pm.makePersistent(buy);
-		AchievementsManager.userValueIncreased(buy);
-		AchievementsManager.someoneBoughtUser(buy);
-		pm.close();
-		response.getWriter().println("Purchase Done");
+    @SuppressWarnings("unchecked")
+    private void buy(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	long userID = Long.parseLong(request.getParameter("userID"));
+	long buyID = Long.parseLong(request.getParameter("buyID"));
+	PersistenceManager pm = PMF.get().getPersistenceManager();
+	Query query = pm.newQuery(User.class);
+	query.setFilter("id == " + userID + " || id == " + buyID);
+	List<User> result = (List<User>) query.execute();
+	query.closeAll();
+	User buyer = null, buy = null;
+	for (User user : result) {
+	    if (user.getId() == userID)
+		buyer = user;
+	    else
+		buy = user;
 	}
+	if (buyer == null)
+	    throw new ServletException("This user doesn't exist");
+	if (buy == null)
+	    throw new ServletException("The user you want to buy doesn't exist");
+	if (buy.getOwner() == userID)
+	    throw new ServletException("You already own the user you want to buy");
+	if (buyer.getMoney() < buy.getValue())
+	    throw new ServletException("You don't have enough money to buy this user");
+	buyer.setMoney(buyer.getMoney() - buy.getValue());
+	pm.makePersistent(buyer);
+	AchievementsManager.userBoughtSomeone(buyer);
+	if (buy.getOwner() > 0) {
+	    query = pm.newQuery(User.class);
+	    query.setFilter("id == " + buy.getOwner());
+	    result = (List<User>) query.execute();
+	    query.closeAll();
+	    if (!result.isEmpty()) {
+		User preOwner = result.get(0);
+		preOwner.setMoney(preOwner.getMoney() + buy.getValue());
+		pm.makePersistent(preOwner);
+	    }
+	}
+	buy.setValue(buy.getValue() * 11 / 10);
+	buy.setOwner(userID);
+	pm.makePersistent(buy);
+	AchievementsManager.userValueIncreased(buy);
+	AchievementsManager.someoneBoughtUser(buy);
+	pm.close();
+	response.getWriter().println("Purchase Done");
+    }
+
+    @SuppressWarnings("unchecked")
+    private void updateStatus(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.setCharacterEncoding("UTF-8");
+	PrintWriter out = response.getWriter();
+
+	long userID = Long.parseLong(request.getParameter("userID"));
+	String status = request.getParameter("status");
+
+	PersistenceManager pm = PMF.get().getPersistenceManager();
+	Query query = pm.newQuery(User.class);
+	query.setFilter("id == " + userID);
+	List<User> result = (List<User>) query.execute();
+	query.closeAll();
+	if (result.isEmpty())
+	    throw new ServletException("This user doesn't exist");
+	User user = result.get(0);
+	user.setStatus(status);
+
+	try {
+	    pm.makePersistent(user);
+	} finally {
+	    pm.close();
+	}
+	out.println("Updated status: " + status);
+
+    }
+
 }
--- Friendizer-AppEngine/src/com/teamagly/friendizer/User.java
+++ Friendizer-AppEngine/src/com/teamagly/friendizer/User.java
@@ -9,127 +9,155 @@ import org.json.*;
 
 @PersistenceCapable
 public class User {
-	@PrimaryKey
-	@Persistent
-	private Long id;
+    @PrimaryKey
+    @Persistent
+    private Long id;
 
-	@Persistent
-	private long owner;
+    @Persistent
+    private long owner;
 
-	@Persistent
-	private long value;
+    @Persistent
+    private long value;
 
-	@Persistent
-	private long money;
+    @Persistent
+    private long money;
 
-	@Persistent
-	private double latitude;
+    @Persistent
+    private double latitude;
 
-	@Persistent
-	private double longitude;
+    @Persistent
+    private double longitude;
 
-	@Persistent
-	private Date since;
+    @Persistent
+    private Date since;
 
-	public User(long id, long owner, long value, long money, double latitude, double longitude, Date since) {
-		this.id = id;
-		this.owner = owner;
-		this.value = value;
-		this.money = money;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.since = since;
+    @Persistent
+    private String status;
+
+    public User(long id, long owner, long value, long money, double latitude, double longitude, Date since, String status) {
+	this.id = id;
+	this.owner = owner;
+	this.value = value;
+	this.money = money;
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.since = since;
+	this.status = status;
+    }
+
+    /**
+     * Constructor for a new user
+     */
+    public User(long id) {
+	this.id = id;
+	this.owner = 0;
+	this.value = 100;
+	this.money = 1000;
+	this.latitude = -1;
+	this.longitude = -1;
+	this.since = new Date();
+	this.status = "";
+    }
+
+    public User(JSONObject obj) throws JSONException {
+	id = obj.getLong("id");
+	owner = obj.getLong("owner");
+	value = obj.getLong("value");
+	money = obj.getLong("money");
+	latitude = obj.getDouble("latitude");
+	longitude = obj.getDouble("longitude");
+	status = obj.getString("status");
+	SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	try {
+	    since = format.parse(obj.getString("since"));
+	} catch (ParseException e) {
+	    throw new JSONException("JSONObject[\"since\"] is not a date.");
 	}
-	
-	public User(JSONObject obj) throws JSONException {
-		id = obj.getLong("id");
-		owner = obj.getLong("owner");
-		value = obj.getLong("value");
-		money = obj.getLong("money");
-		latitude = obj.getDouble("latitude");
-		longitude = obj.getDouble("longitude");
-		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		try {
-			since = format.parse(obj.getString("since"));
-		} catch (ParseException e) {
-			throw new JSONException("JSONObject[\"since\"] is not a date.");
-		}
-	}
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+	return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+	this.id = id;
+    }
 
-	public long getOwner() {
-		return owner;
-	}
+    public long getOwner() {
+	return owner;
+    }
 
-	public void setOwner(long owner) {
-		this.owner = owner;
-	}
+    public void setOwner(long owner) {
+	this.owner = owner;
+    }
 
-	public long getValue() {
-		return value;
-	}
+    public long getValue() {
+	return value;
+    }
 
-	public void setValue(long value) {
-		this.value = value;
-	}
+    public void setValue(long value) {
+	this.value = value;
+    }
 
-	public long getMoney() {
-		return money;
-	}
+    public long getMoney() {
+	return money;
+    }
 
-	public void setMoney(long money) {
-		this.money = money;
-	}
+    public void setMoney(long money) {
+	this.money = money;
+    }
 
-	public double getLatitude() {
-		return latitude;
-	}
+    public double getLatitude() {
+	return latitude;
+    }
 
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
+    public void setLatitude(double latitude) {
+	this.latitude = latitude;
+    }
 
-	public double getLongitude() {
-		return longitude;
-	}
+    public double getLongitude() {
+	return longitude;
+    }
 
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
+    public void setLongitude(double longitude) {
+	this.longitude = longitude;
+    }
 
-	public Date getSince() {
-		return since;
-	}
+    public Date getSince() {
+	return since;
+    }
 
-	public void setSince(Date since) {
-		this.since = since;
+    public void setSince(Date since) {
+	this.since = since;
+    }
+
+    public String getStatus() {
+	return status;
+    }
+
+    public void setStatus(String status) {
+	this.status = status;
+    }
+
+    public JSONObject toJSONObject() {
+	JSONObject obj = new JSONObject();
+	try {
+	    obj.put("id", id);
+	    obj.put("owner", owner);
+	    obj.put("value", value);
+	    obj.put("money", money);
+	    obj.put("latitude", latitude);
+	    obj.put("longitude", longitude);
+	    obj.put("status", status);
+	    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	    obj.put("since", format.format(since));
+	} catch (JSONException e) {
 	}
-	
-	public JSONObject toJSONObject() {
-		JSONObject obj = new JSONObject();
-		try {
-			obj.put("id", id);
-			obj.put("owner", owner);
-			obj.put("value", value);
-			obj.put("money", money);
-			obj.put("latitude", latitude);
-			obj.put("longitude", longitude);
-			SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-			obj.put("since", format.format(since));
-		} catch (JSONException e) {
-		}
-		return obj;
-	}
-	
-	@Override
-	public String toString() {
-		return toJSONObject().toString();
-	}
+	return obj;
+    }
+
+    @Override
+    public String toString() {
+	return toJSONObject().toString();
+    }
 }
