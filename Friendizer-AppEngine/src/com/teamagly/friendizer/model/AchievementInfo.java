7ea5d3245980a1fff2e4c3ee4093ce4a764c73bd
guy.rosin <guy.rosin@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-05-25 22:36:20 +0000

    Added Friendizer-AppEngine

---
+++ Friendizer-AppEngine/src/com/teamagly/friendizer/model/AchievementInfo.java
@@ -0,0 +1,58 @@
+package com.teamagly.friendizer.model;
+
+import org.json.*;
+
+public class AchievementInfo {
+	private Achievement achv;
+	private boolean earned;
+	
+	public AchievementInfo(Achievement achv, boolean earned) {
+		this.achv = achv;
+		this.earned = earned;
+	}
+	
+	public AchievementInfo(JSONObject obj) throws JSONException {
+		long id = obj.getLong("id");
+		String title = obj.getString("title");
+		String description = obj.getString("description");
+		String iconRes = obj.getString("iconRes");
+		int reward = obj.getInt("reward");
+		achv = new Achievement(id, title, description, iconRes, reward);
+		earned = obj.getBoolean("earned");
+	}
+
+	public Achievement getAchv() {
+		return achv;
+	}
+
+	public void setAchv(Achievement achv) {
+		this.achv = achv;
+	}
+
+	public boolean isEarned() {
+		return earned;
+	}
+
+	public void setEarned(boolean earned) {
+		this.earned = earned;
+	}
+	
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("id", achv.getId());
+			obj.put("title", achv.getTitle());
+			obj.put("description", achv.getDescription());
+			obj.put("iconRes", achv.getIconRes());
+			obj.put("reward", achv.getReward());
+			obj.put("earned", earned);
+		} catch (JSONException e) {
+		}
+		return obj;
+	}
+	
+	@Override
+	public String toString() {
+		return toJSONObject().toString();
+	}
+}
---
+++ Friendizer-AppEngine/src/com/teamagly/friendizer/model/Message.java
@@ -0,0 +1,134 @@
+package com.teamagly.friendizer.model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
+@PersistenceCapable
+public class Message {
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Long id;
+
+    @Persistent
+    private long source;
+
+    @Persistent
+    private long destination;
+
+    @Persistent
+    private String text;
+
+    @Persistent
+    private Date time;
+
+    @Persistent
+    private boolean unread;
+
+    public Message() {
+
+    }
+
+    public Message(long source, long destination, String text) {
+	super();
+	this.source = source;
+	this.destination = destination;
+	this.text = text;
+	this.unread = true;
+	this.time = new Date();
+    }
+
+    public Message(JSONObject obj) throws JSONException {
+	// JSONObject obj = new JSONObject(jsonString);
+	this.id = obj.getLong("id");
+	this.source = obj.getLong("source");
+	this.destination = obj.getLong("destination");
+	this.text = obj.getString("text");
+	this.unread = obj.getBoolean("unread");
+	SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	try {
+	    this.time = format.parse(obj.getString("time"));
+	} catch (ParseException e) {
+	    throw new JSONException("JSONObject[\"time\"] is not a date.");
+	}
+    }
+
+    @Override
+    public String toString() {
+	return toJSONObject().toString();
+    }
+
+    public JSONObject toJSONObject() {
+	JSONObject obj = new JSONObject();
+	try {
+	    obj.put("id", id);
+	    obj.put("source", source);
+	    obj.put("destination", destination);
+	    obj.put("text", text);
+	    obj.put("unread", unread);
+	    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	    obj.put("time", format.format(time));
+	} catch (JSONException e) {
+	}
+	return obj;
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long key) {
+	this.id = key;
+    }
+
+    public long getSource() {
+	return source;
+    }
+
+    public void setSource(long source) {
+	this.source = source;
+    }
+
+    public long getDestination() {
+	return destination;
+    }
+
+    public void setDestination(long destination) {
+	this.destination = destination;
+    }
+
+    public String getText() {
+	return text;
+    }
+
+    public void setText(String text) {
+	this.text = text;
+    }
+
+    public boolean isUnread() {
+	return unread;
+    }
+
+    public void setUnread(boolean unread) {
+	this.unread = unread;
+    }
+
+    public Date getTime() {
+	return time;
+    }
+
+    public void setTime(Date time) {
+	this.time = time;
+    }
+
+}
