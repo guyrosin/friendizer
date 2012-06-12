54f2bbecab782b0b6d0c770e5465173d634a940a
aaa.charikar <aaa.charikar@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-06-12 22:16:52 +0000

    Added the gifts servlets.

---
+++ Friendizer-AppEngine/src/com/teamagly/friendizer/model/Gift.java
@@ -0,0 +1,87 @@
+package com.teamagly.friendizer.model;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@PersistenceCapable
+public class Gift {
+	@PrimaryKey
+	@Persistent
+	private Long id;
+	
+	@Persistent
+    private String name;
+	
+	@Persistent
+    private String iconRes;
+    
+	@Persistent
+	private int value;
+
+	public Gift(Long id, String name, String iconRes, int value) {
+		this.id = id;
+		this.name = name;
+		this.iconRes = iconRes;
+		this.value = value;
+	}
+	
+	public Gift(JSONObject obj) throws JSONException {
+		id = obj.getLong("id");
+		name = obj.getString("name");
+		iconRes = obj.getString("iconRes");
+		value = obj.getInt("value");
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIconRes() {
+		return iconRes;
+	}
+
+	public void setIconRes(String iconRes) {
+		this.iconRes = iconRes;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("id", id);
+			obj.put("name", name);
+			obj.put("iconRes", iconRes);
+		    obj.put("value", value);
+		} catch (JSONException e) {
+		}
+		return obj;
+	}
+	
+	@Override
+	public String toString() {
+		return toJSONObject().toString();
+	}
+}
