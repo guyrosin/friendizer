d811de20403555f0119fbeb2d9b2bbb3435a7e86
yardenron4@gmail.com <yardenron4@gmail.com@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-09-03 09:44:47 +0000

    Added a dynamic misssion.

--- trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/LocationManager.java
+++ trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/LocationManager.java
@@ -12,8 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.android.gcm.server.Message;
 import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.teamagly.friendizer.Notifications.NotificationType;
 import com.teamagly.friendizer.model.User;
+import com.teamagly.friendizer.model.Action;
 import com.teamagly.friendizer.model.UserMatching;
 
 @SuppressWarnings("serial")
@@ -44,6 +47,70 @@ public class LocationManager extends HttpServlet {
 		user.setLatitude(latitude);
 		user.setLongitude(longitude);
 		user.setSince(new Date());
+		
+		// TODO: Put it in a new thread
+		
+		/* Sending notification to a nearby user who bought me in the past (but didn't buy me for a week) */
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.MINUTE, -30);
+		Date updated = cal.getTime();
+		query = pm.newQuery(User.class);
+		query.setFilter("since > updatedDate");
+		query.declareParameters("java.util.Date updatedDate");
+		// Get all the online users from the database
+		result = (List<User>) query.execute(updated);
+		query.closeAll();
+		
+		Query query2;
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(new Date());
+		// Set a date of a week ago
+		cal2.add(Calendar.DAY_OF_MONTH, -7);
+		Date updated2 = cal2.getTime();
+		List<Action> result2;
+		
+		/*
+		 * The loop goes over the users and check only the nearby users
+		 */
+		for (User nearbyUser : result) {
+			if ((nearbyUser.getId() != userID) && (user.getLatitude() - nearbyUser.getLatitude()) * (user.getLatitude() - nearbyUser.getLatitude()) + 
+					(user.getLongitude() - nearbyUser.getLongitude()) * (user.getLongitude() - nearbyUser.getLongitude()) <= 1000) {
+				
+				query2 = pm.newQuery(Action.class);
+				query2.setFilter("date < updatedDate && (buyerID == nearbyUserID && boughtID == userID)");
+				query2.declareParameters("java.util.Date updatedDate, long userID, long nearbyUserID");
+				
+				// Check if the nearby user bought the current user before one week (or more)
+				result2 = (List<Action>) query2.execute(updated2,userID,nearbyUser.getId());
+				
+				query2.closeAll();
+				
+				// If he bought him in the past
+				if (!result2.isEmpty())
+				{					
+					query2 = pm.newQuery(Action.class);
+					query2.setFilter("date > updatedDate && (buyerID == nearbyUserID && boughtID == userID)");
+					query2.declareParameters("java.util.Date updatedDate, long userID, long nearbyUserID");
+					
+					// Check if the nearby user bought the current user in the last week
+					result2 = (List<Action>) query2.execute(updated2,userID,nearbyUser.getId());
+					
+					query2.closeAll();
+					
+					// If he didn't buy him in the last week (didn't buy him for a long time)
+					if (result2.isEmpty())
+					{					
+						// Send a notification to the nearby user suggesting to buy the user again
+						Message msg = new Message.Builder().addData("type", NotificationType.NEARBY.toString())
+								.addData(Util.USER_ID, String.valueOf(userID)).addData("text", Notifications.NEARBY_MSG).build();
+						SendMessage.sendMessage(nearbyUser.getId(), msg);
+					}
+				}
+			}
+		}
+		
 		pm.close();
 		response.getWriter().println("The user location was changed");
 	}
