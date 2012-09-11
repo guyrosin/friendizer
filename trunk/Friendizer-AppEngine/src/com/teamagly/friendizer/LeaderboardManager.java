30b8aa6a29d2a0ad26ef489940fb207abefea337
guy.rosin <guy.rosin@893eca86-03a1-db22-0987-cda46d1ef62f>
2012-09-11 11:06:28 +0000

    Nothing much

--- trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/LeaderboardManager.java
+++ trunk/Friendizer-AppEngine/src/com/teamagly/friendizer/LeaderboardManager.java
@@ -26,6 +26,7 @@ public class LeaderboardManager extends HttpServlet {
 
 	@SuppressWarnings("unchecked")
 	private void getLeaderboard(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
 		String type = request.getParameter("type");
 
